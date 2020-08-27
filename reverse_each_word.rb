c68dca49c61a8156e0f8e1cefbdbe904c3774378
Taylor Sieling <taylorasieling@gmail.com>
2020-08-27 01:58:09 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence)
   array = sentence.split(" ")
   new_array = []
   array.each do |word|
-    new_array << sentence.reverse
+    new_array << word.reverse
   end
   new_array.join(" ")
 end
